cc025ca431fd82e5ee05f1516fb978e6f4088f83
ryoheimatsumoto <ryo828.kgu@gmail.com>
2019-12-29 21:26:24 +0900

    d

--- scraping_bot/urls.py
+++ scraping_bot/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bot_scraping/', include('bot_scraping.urls'))
+    path('bot_scraping', include('bot_scraping.urls'))
 ]
